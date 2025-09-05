91dc1f61a3e3f35596a7d7af3daac9cf920100b6
Sivakumar089 <kumarsiva200426@gmail.com>
2025-09-05 22:53:24 +0530

    react native app

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+modeule.exports=function(api){
+    api.cache(true);
+    return{
+        presets:[
+            ["babel-present-expo",{jsxImportSource:"nativewind"}],
+            "nativewind/babel",
+        ],
+    };
+};
